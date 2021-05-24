1ba95cdbf1c7985b24b7e143c1926ac06c73e72f
V. Avalos <vavalosla@gmail.com>
2021-05-23 17:58:46 -0700

    new: decalared new floats and new valid_coords funtion

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,27 @@
 
 using namespace std::experimental;
 
+int validate_cords(float &c)
+{
+    while(true)
+    {
+        cin >> c;
+        while(!(c >= 0 && c <= 100))
+        {
+            // We clear the input so this loop doesn't stay infinite
+            cin.clear();
+            // We use ignore to allow us to take input again
+            cin.ignore();
+            cout << "Invalid. Enter a valid coordinate from 0 - 100 : " << std::endl;
+            cin >> c;
+        }
+    
+        return c;
+    }
+    
+}
+
+
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
     std::ifstream is{path, std::ios::binary | std::ios::ate};
@@ -52,15 +73,25 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+
+    // user input for these values using 
+
+    float start_x, start_y, end_x, end_y;
+    cout << "Enter the starting 'x' coordinate : " << std::endl;
+    validate_cords(start_x);
+    cout << "Enter the starting 'y' coordinate : " << std::endl;
+    validate_cords(start_y);
+    cout << "Enter the ending 'x' coordinate : " << std::endl;
+    validate_cords(end_x);
+    cout << "Enter the ending 'y' coordinate : " << std::endl;
+    validate_cords(end_y);
+
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
